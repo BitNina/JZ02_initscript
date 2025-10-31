137ead7f896c2a4d0117d254cd1ef1557e518d72
BitNina <15542476970@163.com>
2025-10-31 15:12:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# wget https://raw.githubusercontent.com/BitNina/JZ02_initscript/refs/heads/main/run.sh && chmod +x run.sh && ./run.sh
 set -e
 cat <<EOF | sudo tee /etc/apt/sources.list
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
@@ -7,6 +7,7 @@ deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib n
 EOF
 
 sudo apt-get update
+sudo apt-get install --reinstall debian-archive-keyring
 sudo apt-get install -y cpufrequtils apt-transport-https ca-certificates
 sudo cpufreq-set -u 1.2ghz
 
